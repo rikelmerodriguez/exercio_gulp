1de054cb40e090e0884b3def48993e460799753d
rikelmerodriguez <rodriguezrikelme17@gmail.com>
2024-05-17 21:16:31 -0300

    setup_gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,57 +3,50 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
-// Caminhos para os arquivos
 const paths = {
     styles: {
-        src: 'src/scss/**/*.scss',
+        src: 'src/sass/**/*.scss',
         dest: 'dist/css'
-    },
-    scripts: {
-        src: 'src/js/**/*.js',
-        dest: 'dist/js'
     },
     images: {
         src: 'src/images/**/*',
         dest: 'dist/images'
+    },
+    scripts: {
+        src: 'src/js/**/*.js',
+        dest: 'dist/js'
     }
 };
 
-// Tarefa de compilação do SASS
 function styles() {
     return gulp.src(paths.styles.src)
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(paths.styles.dest));
 }
 
-// Tarefa de compressão de imagens
 function images() {
     return gulp.src(paths.images.src)
         .pipe(imagemin())
         .pipe(gulp.dest(paths.images.dest));
 }
 
-// Tarefa de compressão de código JavaScript
 function scripts() {
     return gulp.src(paths.scripts.src)
         .pipe(uglify())
         .pipe(gulp.dest(paths.scripts.dest));
 }
 
-// Tarefa para observar mudanças nos arquivos
-function watch() {
+function watchFiles() {
     gulp.watch(paths.styles.src, styles);
-    gulp.watch(paths.scripts.src, scripts);
     gulp.watch(paths.images.src, images);
+    gulp.watch(paths.scripts.src, scripts);
 }
 
-// Definindo as tarefas disponíveis
-const build = gulp.series(gulp.parallel(styles, images, scripts));
-gulp.task('build', build);
-gulp.task('watch', watch);
+const build = gulp.series(gulp.parallel(styles, images, scripts), watchFiles);
 
 exports.styles = styles;
 exports.images = images;
 exports.scripts = scripts;
-exports.watch = watch;
+exports.watch = watchFiles;
 exports.build = build;
+exports.default = build;
